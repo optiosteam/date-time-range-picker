67f262398a3c9c3a42b9aebe791050a8bd30f7c5
Thomas Vanderhaeghen <thomas@optios.net>
2020-03-07 16:13:27 +0100

    Basic build and test setup

---
+++ src/__tests__/index.spec.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import DateTimeRangePicker from '../index'
+
+configure({ adapter: new Adapter() });
+
+describe('DateTimeRangePicker', () => {
+    it('should render correctly', () => {
+        const component = shallow(<DateTimeRangePicker />);
+
+        expect(component).toMatchSnapshot();
+    });
+});
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const DateTimeRangePicker: React.FunctionComponent<{}> = () => {
+    return <div>Typescript react date time picker!</div>
+}
+
+export default DateTimeRangePicker
+
